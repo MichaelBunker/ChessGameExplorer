a079e4a499c06171b192a2a345f3c6cf42ced755
Bunker <student@epicodus.com>
2015-10-28 18:21:51 -0700

    add edit function to Games class

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -17,6 +17,19 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
   end
 
+  def edit
+    @game = Game.find(params[:id])
+  end
+
+  def update
+    @game = Game.find(params[:id])
+    if @game.update(game_params)
+      redirect_to game_path(@game)
+    else
+      render :edit
+    end
+  end
+
 private
   def game_params
     params.require(:game).permit(:notation)
