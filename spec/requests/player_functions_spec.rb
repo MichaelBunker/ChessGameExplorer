5d12b967fe17284502c63d3357710f78fb59805f
Bunker <student@epicodus.com>
2015-10-28 18:32:41 -0700

    add integration test for editing Players function

--- spec/requests/player_functions_spec.rb
+++ spec/requests/player_functions_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "PlayerFunctions", type: :request do
+
+  before do
+    User.create(email: "bunker@gmail.com", password: "password123", password_confirmation: "password123")
+    visit root_path
+    click_link "Login"
+    fill_in "Email", with: "bunker@gmail.com"
+    fill_in "Password", with: "password123"
+    click_on "Log in"
+  end
+
   describe 'the add a player process' do
     it "adds a player" do
-      User.create(email: "bunker@gmail.com", password: "password123", password_confirmation: "password123")
-      visit root_path
-      click_link "Login"
-      fill_in "Email", with: "bunker@gmail.com"
-      fill_in "Password", with: "password123"
-      click_on "Log in"
       click_link "Add a new Player"
       fill_in "Name", with: "Jack"
       fill_in "Rating", with: "2000"
@@ -17,4 +21,20 @@ RSpec.describe "PlayerFunctions", type: :request do
       expect(page).to have_content "rating: 2000"
     end
   end
+
+  describe 'the edit player process' do
+    it 'Edits a player' do
+      click_link "Add a new Player"
+      fill_in "Name", with: "Jack"
+      fill_in "Rating", with: "2000"
+      click_on "Add"
+      click_on "Edit this player"
+      fill_in "Name", with: "Bob"
+      fill_in "Rating", with: "1000"
+      click_on "Add"
+      expect(page).to have_content "Page for Bob"
+      expect(page).to have_content "rating: 1000"
+    end
+  end
+
 end
