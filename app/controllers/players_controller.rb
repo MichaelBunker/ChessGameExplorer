8bbb615573458f87e2b1e5bda45c4e7868fcda21
Bunker <student@epicodus.com>
2015-10-28 17:52:14 -0700

    add new player function and routes

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -7,11 +7,26 @@ class PlayersController < ApplicationController
   end
 
   def new
-    
+    @player = Player.new
   end
 
   def create
+    @player = Player.new(player_params)
+    if @player.save
+      redirect_to player_path(@player)
+    else
+      render :new
+    end
+  end
 
+  def show
+    @player = Player.find(params[:id])
+  end
+
+  
+private
+  def player_params
+    params.require(:player).permit(:name, :rating)
   end
 
 end
