a69ca17c5ef3c315b8636427f96f1bbaf15488d3
Bunker <student@epicodus.com>
2015-10-28 18:36:07 -0700

    add edit player route and functionality

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -23,7 +23,20 @@ class PlayersController < ApplicationController
     @player = Player.find(params[:id])
   end
 
-  
+  def edit
+    @player = Player.find(params[:id])
+  end
+
+  def update
+    @player = Player.find(params[:id])
+    if @player.update(player_params)
+      redirect_to player_path(@player)
+    else
+      render :edit
+    end
+  end
+
+
 private
   def player_params
     params.require(:player).permit(:name, :rating)
