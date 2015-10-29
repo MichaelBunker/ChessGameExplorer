2e24498b88d11788a38858416dabea8557f538e4
Bunker <student@epicodus.com>
2015-10-28 18:13:57 -0700

    add show page for individual games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,10 +7,14 @@ class GamesController < ApplicationController
   def create
     @game = Game.new(game_params)
     if @game.save
-      redirect_to players_path
+      redirect_to game_path(@game)
     else
       render :new
     end
+  end
+
+  def show
+    @game = Game.find(params[:id])
   end
 
 private
