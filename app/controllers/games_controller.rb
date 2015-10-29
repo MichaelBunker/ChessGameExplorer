10345c173557eea7e9bcd007e89b9214dc60e0c5
Bunker <student@epicodus.com>
2015-10-28 17:44:26 -0700

    add create Game function and route

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,6 +6,15 @@ class GamesController < ApplicationController
 
   def create
     @game = Game.new(game_params)
+    if @game.save
+      redirect_to players_path
+    else
+      render :new
+    end
   end
 
+private
+  def game_params
+    params.require(:game).permit(:notation)
+  end
 end
