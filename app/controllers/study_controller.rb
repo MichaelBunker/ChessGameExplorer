d70e03f1de008877cfddd86211b2f8c57678a9f1
Bunker <student@epicodus.com>
2015-11-05 09:28:57 -0800

    create study page/route and controller for researching chess info on wikipedia.

---
+++ app/controllers/study_controller.rb
@@ -0,0 +1,11 @@
+class StudyController < ApplicationController
+
+  def show
+
+  end
+
+  def index
+
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   root 'players#index'
   resources :players
 
   resources :games
+
+  resources :study
 
 
 end
