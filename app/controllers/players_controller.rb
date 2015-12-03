857258acab165f63330cf10ae0ae3caae56ca94e
Bunker <student@epicodus.com>
2015-12-03 13:45:58 -0800

    clean up syntax for players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -10,36 +10,39 @@ class PlayersController < ApplicationController
     moves = params[:moves]
     if moves
       @games = Game.where("notation LIKE ?", "#{pgn}%")
-      @arr = []
+      @moves_a = []
       if pgn.include?("1. e4")
         if pgn == "1. e4"
           moves.each do |move|
             number = @games.where("notation LIKE ?", "#{pgn} #{move}%")
             if number == 0
-              @arr << 0
+              @moves_a << 0
             else
-              @arr << number.length
+              @moves_a << number.length
             end
           end
         end
         # I think a better solution to all of this might be to pass in which color turn it is. If white's turn, search and add number. Since black will already have it in the string.
+        #bug where second move by black is caught in this section.
+        # IE. (notation LIKE '1. e4 d5 2. exd5 3. Qxd5%') instead of (notation LIKE '1. e4 d5 2. exd5 Qxd5%')
         number = pgn.scan(/\d\./)
         convert = number.last.to_i
         addToGame = convert + 1
         newStringNum = addToGame.to_s
         pgn.concat(" " + newStringNum + ". ")
+        pry
       end
       moves.each do |move|
           number = @games.where("notation LIKE ?", "#{pgn}#{move}%")
         if number == 0
-          @arr << 0
+          @moves_a << 0
         else
-          @arr << number.length
+          @moves_a << number.length
         end
       end
       respond_with do |format|
         format.html
-        format.json { render :json => {"moves": moves, "next_move": @arr} }
+        format.json { render :json => {"moves": moves, "next_move": @moves_a} }
       end
     end
   end
