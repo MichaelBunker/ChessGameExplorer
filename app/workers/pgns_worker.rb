fdb849a968ad4729b15eab289b584f78123ec14b
Bunker <michaelbunker1@gmail.com>
2015-12-13 01:32:34 -0800

    isolate pgn notation in mass upload of games

--- app/workers/pgns_worker.rb
+++ app/workers/pgns_worker.rb
@@ -9,6 +9,16 @@ class PgnsWorker
     unparsed_games = games.read
     split_games = unparsed_games.split(/\r\n\r\n\r\n/)
     puts split_games.last
+    player = Player.find(1)
+    split_games.each do |game|
+      game_notation = game.split(/\r\n\r\n/).pop
+      game_notation.gsub!(/(\d+\.)/, '\1 ')
+      puts game_notation
+      game = Game.new
+      game = Game.new(notation: game_notation)
+      game.players.push(player)
+      game.save
+    end
   end
 
 end
