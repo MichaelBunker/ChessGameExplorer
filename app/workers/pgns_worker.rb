75fd881cd5c701c1c18889c73a7edca49a5b2e59
Bunker <michaelbunker1@gmail.com>
2015-12-14 00:05:55 -0800

    refactor sidekiq worker to account for rating and file format

--- app/workers/pgns_worker.rb
+++ app/workers/pgns_worker.rb
@@ -14,20 +14,20 @@ class PgnsWorker
     # ((?<=Black )"\w{3,15}")
     split_games.each do |game|
       # split game notation from rest of PGN info, add spaces to normalize data, remove extra line breaks.
-      white = game.scan(/(?<=White )"\w{3,15}"/).join("")
-      white.gsub!("\"", "")
-      white_rating = game.scan(/(?<=WhiteElo\s)"\d{2,4}"/).join("")
-      rating = white_rating.to_i
-      puts white_rating
-      puts "--------------------------------"
-      puts rating
-      black = game.scan(/(?<=Black )"\w{3,15}"/).join("")
-      black.gsub!("\"", "")
-      black_rating = game.scan(/(?<=BlackElo\s)"\d{2,4}"/).join("").to_i
+      game = game.gsub(/"/, "").gsub(/ /, ",")
+
+      white = game.scan(/(?<=White,)\w{3,15}/).join("")
+      white_rating = game.scan(/(?<=WhiteElo,)\d{2,4}/).join("").to_i
+
+      black = game.scan(/(?<=Black,)\w{3,15}/).join("")
+      black_rating = game.scan(/(?<=BlackElo,)\d{2,4}/).join("").to_i
+
       player1 = Player.find_or_create_by(name: white, rating: white_rating)
       player2 = Player.find_or_create_by(name: black, rating: black_rating)
+
       player1.save
       player2.save
+
       game_notation = game.split(/\r\n\r\n/).pop
       game_notation.gsub!(/(\d+\.)/, '\1 ')
       game_notation.gsub!(/\r\n/, '')
@@ -38,6 +38,5 @@ class PgnsWorker
       game.save
     end
   end
-
 end
 # rough estimate. 1k smallish games of just notation parsed and added -  done: 11.752 sec.
