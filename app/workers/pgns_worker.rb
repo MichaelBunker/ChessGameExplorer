d1903102b60345ad1ef759dc5cf6aca2a76c6974
Bunker <michaelbunker1@gmail.com>
2015-12-13 18:16:54 -0800

    add player rating into regex search of PGN file

--- app/workers/pgns_worker.rb
+++ app/workers/pgns_worker.rb
@@ -14,12 +14,18 @@ class PgnsWorker
     # ((?<=Black )"\w{3,15}")
     split_games.each do |game|
       # split game notation from rest of PGN info, add spaces to normalize data, remove extra line breaks.
-      white = game.scan(/(?<=White )"\w{3,15}"/)
-      black = game.scan(/(?<=Black )"\w{3,15}"/)
-      puts white
-      player1 = Player.find_or_create_by(name: white, rating: 1000)
-      puts player1.name
-      player2 = Player.find_or_create_by(name: black, rating: 1000)
+      white = game.scan(/(?<=White )"\w{3,15}"/).join("")
+      white.gsub!("\"", "")
+      white_rating = game.scan(/(?<=WhiteElo\s)"\d{2,4}"/).join("")
+      rating = white_rating.to_i
+      puts white_rating
+      puts "--------------------------------"
+      puts rating
+      black = game.scan(/(?<=Black )"\w{3,15}"/).join("")
+      black.gsub!("\"", "")
+      black_rating = game.scan(/(?<=BlackElo\s)"\d{2,4}"/).join("").to_i
+      player1 = Player.find_or_create_by(name: white, rating: white_rating)
+      player2 = Player.find_or_create_by(name: black, rating: black_rating)
       player1.save
       player2.save
       game_notation = game.split(/\r\n\r\n/).pop
